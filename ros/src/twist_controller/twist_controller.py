a7afdd774a1cf3369d7479fef5d3da6a80e1bc0d
Michael P. Scherer <scherer@protonmail.ch>
2017-11-27 22:10:38 -0800

    Fixed bugs in velocity controller - Range on acceleration control was limited to [0,1], preventing braking - Speed error was squared, and thus could never be negative

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -9,7 +9,7 @@ ONE_MPH = 0.44704
 class Controller(object):
     def __init__(self, wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle, kp, ki, kd, max_speed):
         self.yawc = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
-        self.pidc = PID(kp, ki, kd, 0, 1)
+        self.pidc = PID(kp, ki, kd, -1, 1)
 
     def control(self, command_twist, current_twist, dt):
         cmd_vel = command_twist.twist.linear.x
@@ -19,7 +19,7 @@ class Controller(object):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
         
-        vel_err = (current_vel - cmd_vel) ** 2
+        vel_err = cmd_vel - current_vel;
         
         if current_vel > 80:
             vel_err *= 2
@@ -27,15 +27,15 @@ class Controller(object):
         accel = self.pidc.step(vel_err, dt)
         steer = self.yawc.get_steering(current_vel, cmd_steer, current_vel)
         
-        rospy.logwarn('curr_steer={:6.3f} cmd_steer={:6.3f} steer={:6.3f}'.format(
-            current_steer, cmd_steer, steer))
+        #rospy.logwarn('curr_steer={:6.3f} cmd_steer={:6.3f} steer={:6.3f}'.format(
+        #    current_steer, cmd_steer, steer))
         
-        rospy.logwarn('curr_vel={:6.3f} cmd_vel={:6.3f} accel={:6.3f}'.format(
-            current_vel, cmd_vel, accel))
+        rospy.logwarn('curr_vel={:+6.3f} cmd_vel={:+6.3f} err={:+6.3f} accel={:+6.3f}'.format(
+            current_vel, cmd_vel, vel_err, accel))
         
         
         if (accel < 0):
-            brake_amount = accel * 100
+            brake_amount = -accel
             return 0, brake_amount, steer
         else:
             return accel, 0, steer
